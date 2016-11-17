b47df24a6c5e93f8fe48eeacb58edc411eeea876
Manish Kumar <mnisk@amazon.com>
2016-11-17 16:41:09 +0530

    Added Empty Android application.

---
+++ app/src/main/java/in/co/manishkumar/pot3/GameActivity.java
@@ -0,0 +1,13 @@
+package in.co.manishkumar.pot3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
